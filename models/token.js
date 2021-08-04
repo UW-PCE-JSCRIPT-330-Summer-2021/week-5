c25da5c860c469bbd5a59e5cad53f00d5e293982
Eddie Lee <inrez00@uw.edu>
2021-08-04 11:32:03 -0700

    rolling back

--- models/token.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const tokenSchema = new mongoose.Schema({
-  token: { type: String, required: true },
-  userId: { type: String, index: true, required: true }
-});
-
-module.exports = mongoose.model("tokens", tokenSchema);
