bda270484bd2d1e52522e29fcfe9020644dcd2a7
Surry <50641921+surryjm@users.noreply.github.com>
2021-08-08 23:28:03 -0700

    working on orders routes

--- daos/order.js
+++ daos/order.js
@@ -1,2 +1,25 @@
-// const Order = require('../models/order');
-// module.exports = {};
+const Order = require('../models/order');
+module.exports = {};
+
+module.exports.createOrder = async (orderObj) => {
+  const order = await Order.create(orderObj);
+  return order;
+}
+
+module.exports.getAllOrders = async () => {
+  const orders = await Order.find().lean();
+  return orders;
+}
+
+module.exports.getOrderByUserId = async (userId) => {
+  const order = await Order.find({ userId: userId }).lean();
+  return order;
+}
+
+module.exports.getOrderByOrderId = async (orderId) => {
+  const order = await Order.aggregate([
+
+  ]);
+  return order;
+}
+
--- routes/orders.js
+++ routes/orders.js
@@ -1,21 +1,56 @@
-// const { Router } = require("express");
-// const router = Router();
+const { Router } = require("express");
+const router = Router();
+const orderDAO = require('../daos/order');
+const itemDAO = require('../daos/item');
+const { isAuthorized, isAdmin } = require('../middleware/auth');
+const { errorHandler } = require('../middleware/error');
 
-
-
+router.use(isAuthorized);
 
 // Create: POST /orders - open to all users
 // Takes an array of item _id values (repeat values can appear). 
 // Order should be created with a total field with the total cost of all the items 
 // from the time the order is placed (as the item prices could change). 
 // The order should also have the userId of the user placing the order.
+router.post("/", async (req, res, next) => {
+  try {
+
+  } catch (e) {
+    next (e)
+  }
+});
 
 // Get my orders: GET /orders - return all the orders made by the user making the request
+router.get("/", isAdmin, async (req, res, next) => {
+  try {
+    if (req.user.isAdmin) {
+      const orders = await orderDAO.getAllOrders();
+      res.json(orders);
+    } else {
+      const ordersByUser = await orderDAO.getOrderByUserId(req.user._id);
+      res.json(ordersByUser);
+    }
+  } catch (e) {
+    next (e)
+  }
+});
 
 // Get an order: GET /order/:id - return an order with the items array 
 // containing the full item objects rather than just their _id. 
 // If the user is a normal user return a 404 if they did not place the order. 
 // An admin user should be able to get any order.
+router.get("/:id", isAdmin, async (req, res, next) => {
+  try {
+    if (!req.user.isAdmin) {
+      res.sendStatus(404);
+    } else {
 
+    }
+  } catch (e) {
+    next (e)
+  }
+});
 
-//module.exports = router;
+router.use(errorHandler);
+
+module.exports = router;
